4e14d40d1b0bf6a11687e6f67e2d56d44f8e904e
apple <apple@apples-MacBook-Pro.local>
2023-12-06 16:39:17 +0530

    functionality changes

--- e-commerce/src/Components/Dresses/Dresses.js
+++ e-commerce/src/Components/Dresses/Dresses.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Dresses.css'
 import shoe from '../../images/shoe.jpeg'
 import coat from '../../images/coat.jpeg'
@@ -6,9 +6,31 @@ import jacket from '../../images/jacket.jpeg'
 import dress from '../../images/dress.jpeg'
 import jeans from '../../images/jeans.jpeg'
 import tShirts from '../../images/t-shirts.jpeg'
-
+import { useState } from 'react'
 
 function Dresses() {
+
+    useEffect(() => {
+        let nodeDressSub = document.querySelectorAll('.dress-sub');
+        console.log(nodeDressSub)
+        console.log(nodeDressSub.length)
+        for(let i=0;nodeDressSub.length-1;i++) {
+            // let nodeImage = nodeDressSub[i].childNodes[0]
+            // console.log(nodeImage)
+            // let box = nodeDressSub[i].childNodes[1]
+            // let box2 = window.getComputedStyle(box, '::after')
+
+            // nodeDressSub[i].addEventListener('mouseenter', (event) => {
+            //     nodeImage.style.backgroundSize = "110%"
+            //     box.style.setProperty('--afterBack', 'scaleX(1)')
+            // })
+
+            // nodeDressSub[i].addEventListener('mouseleave', (event) => {
+            //     nodeImage.style.backgroundSize = "100%"
+            //     box.style.setProperty('--afterBack', 'scaleX(0)')
+            // })
+        }
+    })
 
     const componentMouseEventDresses = (headClassName, imageClass, mouseEvent) => {
         try {
@@ -28,27 +50,27 @@ function Dresses() {
 
     return (
         <div className="dresses">
-            <a onMouseOver={() => { componentMouseEventDresses('.head', '.image-1','over') }} onMouseLeave={() => { componentMouseEventDresses('.head', '.image-1','leave') }} className="dress-sub">
+            <a className="dress-sub">
                 <div className="image-1"></div>
                 <h2 className='head' >Coats</h2>
             </a>
-            <a onMouseOver={() => { componentMouseEventDresses('.head-2', '.image-2','over') }} onMouseLeave={() => { componentMouseEventDresses('.head-2', '.image-2','leave') }} className="dress-sub">
+            <a className="dress-sub">
                 <div className="image-2"></div>
-                <h2 className='head-2' >Jackets</h2>
+                <h2 className='head' >Jackets</h2>
             </a>
-            <a onMouseOver={() => { componentMouseEventDresses('.head-3', '.image-3','over') }} onMouseLeave={() => { componentMouseEventDresses('.head-3', '.image-3','leave') }} className="dress-sub">
+            <a className="dress-sub">
                 <div className="image-3"></div>
                 <h2 className='head-3' >Dresses</h2>
             </a>
-            <a onMouseOver={() => { componentMouseEventDresses('.head-4', '.image-4','over') }} onMouseLeave={() => { componentMouseEventDresses('.head-4', '.image-4','leave') }} className="dress-sub">
+            <a className="dress-sub">
                 <div className="image-4"></div>
                 <h2 className='head-4' >Shoes</h2>
             </a>
-            <a onMouseOver={() => { componentMouseEventDresses('.head-5', '.image-5','over') }} onMouseLeave={() => { componentMouseEventDresses('.head-5', '.image-5','leave') }} className="dress-sub">
+            <a className="dress-sub">
                 <div className="image-5"></div>
                 <h2 className='head-5' >Jeans</h2>
             </a>
-            <a onMouseOver={() => { componentMouseEventDresses('.head-6', '.image-6','over') }} onMouseLeave={() => { componentMouseEventDresses('.head-6', '.image-6','leave') }} className="dress-sub">
+            <a className="dress-sub">
                 <div className="image-6"></div>
                 <h2 className='head-6' >T-shirts</h2>
             </a>
